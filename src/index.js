5b47bf62163c3147d3593f5fc2ab64e9a76bbd6c
yurihellmann <yurihellmann88@gmail.com>
2024-06-28 13:13:18 -0300

    projeto_finalizado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-let nomePersonagem = "Yuri"
-let quantidadeExp = 12001
+let nomePersonagem = ""
+let quantidadeExp = 6000
 let eloPersonagem = ""
 
 switch(true){
@@ -10,29 +10,36 @@ switch(true){
     case quantidadeExp > 1000:
     case quantidadeExp <= 2000:
         eloPersonagem = "Bronze"
+        break
 
     case quantidadeExp > 2000:
     case quantidadeExp <= 5000:
         eloPersonagem = "Prata"
+        break
 
     case quantidadeExp > 5000:
     case quantidadeExp <= 7000:
         eloPersonagem = "Ouro"
+        break
 
     case quantidadeExp > 7000:
     case quantidadeExp <= 8000:
         eloPersonagem = "Platina"
+        break
 
     case quantidadeExp > 8000:
     case quantidadeExp <= 9000:
         eloPersonagem = "Ascendente"
+        break
 
     case quantidadeExp > 9000:
     case quantidadeExp <= 10000:
         eloPersonagem = "Imortal"
+        break
 
     case quantidadeExp > 10000:
         eloPersonagem = "Radiante"
+        break
 }
 
 console.log("O herói de nome **" + nomePersonagem + "** está no nível de **" + eloPersonagem + "**")
