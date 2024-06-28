d8f5291fd2c360493381c4561a4abad3d18155ff
yurihellmann <yurihellmann88@gmail.com>
2024-06-28 13:10:51 -0300

    projeto-finalizado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-let nome = "Yuri"
+let nomePersonagem = "Yuri"
+let quantidadeExp = 12001
+let eloPersonagem = ""
 
-console.log("oi" + nome)
+switch(true){
+    case quantidadeExp <= 1000:
+        eloPersonagem = "Ferro"
+        break
+
+    case quantidadeExp > 1000:
+    case quantidadeExp <= 2000:
+        eloPersonagem = "Bronze"
+
+    case quantidadeExp > 2000:
+    case quantidadeExp <= 5000:
+        eloPersonagem = "Prata"
+
+    case quantidadeExp > 5000:
+    case quantidadeExp <= 7000:
+        eloPersonagem = "Ouro"
+
+    case quantidadeExp > 7000:
+    case quantidadeExp <= 8000:
+        eloPersonagem = "Platina"
+
+    case quantidadeExp > 8000:
+    case quantidadeExp <= 9000:
+        eloPersonagem = "Ascendente"
+
+    case quantidadeExp > 9000:
+    case quantidadeExp <= 10000:
+        eloPersonagem = "Imortal"
+
+    case quantidadeExp > 10000:
+        eloPersonagem = "Radiante"
+}
+
+console.log("O herói de nome **" + nomePersonagem + "** está no nível de **" + eloPersonagem + "**")
